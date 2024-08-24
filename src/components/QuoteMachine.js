0edc3b0fb2581de868e2d91ec58f57888694f9e8
unknown <royajamal2003@gmail.com>
2024-08-24 03:21:08 -0700

    QuoteMachine.js file linters error solved

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './QuoteMachine.css';
 
 const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,8 +9,8 @@ class App extends React.Component {
       currentquote: '',
       currentAuthor: '',
     };
+  
   }
-
   componentDidMount() {
     this.getRandomQuote();
   }
@@ -75,5 +74,6 @@ class App extends React.Component {
     );
   }
 }
+
 
 export default App;
